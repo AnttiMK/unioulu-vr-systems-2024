3d00bb6fcd00e42f4196a877929883aafd0bae80
epelix <90451715+eemeliHuotari@users.noreply.github.com>
2024-12-10 18:29:00 +0200

    added scene 4

--- My project/Assets/Scripts/ReturnToMenu.cs
+++ My project/Assets/Scripts/ReturnToMenu.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class ReturnToMenu : MonoBehaviour
 {
-    public void Return()
+   
+    public InputActionReference action;
+
+    void Start()
     {
+        action.action.Enable();
+        action.action.performed += (ctx) =>
+        {
         SceneManager.LoadScene(0);
+        };
     }
 }
